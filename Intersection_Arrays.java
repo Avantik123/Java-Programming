31906658d386d627d53e0605aaea24caa6637f18
Avantika Tayde <103492326+AvantikaTayde123@users.noreply.github.com>
2024-08-16 10:50:00 +0530

    Add files via upload

---
+++ Intersection_Arrays.java
@@ -0,0 +1,18 @@
+public class Intersection_Arrays {
+    public static void main(String[] args) {
+      int  a[] = {4,9,7,8,2};
+      int  b[] = {5,1,4,7,3,9};
+
+      for(int i=0;i<a.length;i++){
+        for(int j=0;j<b.length;j++){
+            if(a[i]==b[j]){
+                System.out.println(a[i]);
+            }
+            else{
+            
+            }
+      }
+    }
+    System.out.println("Not Common Element Found");
+    }
+}
