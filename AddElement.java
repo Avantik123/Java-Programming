c5e70cc8f7e35c168776262fb7ed210f0ebcadd8
Avantika Tayde <103492326+AvantikaTayde123@users.noreply.github.com>
2024-08-22 13:40:38 +0530

    Update AddElement.java

--- AddElement.java
+++ AddElement.java
@@ -20,29 +20,4 @@ public class AddElement {
 }
     }
 }
-/*public class AddElementToArray {
-    public static void main(String[] args) {
-        // Original array
-        int[] originalArray = {1, 2, 3};
-        
-        // Element to be added
-        int newElement = 4;
-        
-        // Create a new array with one extra space
-        int[] newArray = new int[originalArray.length + 1];
-        
-        // Copy elements from the original array to the new array using a for loop
-        for (int i = 0; i < originalArray.length; i++) {
-            newArray[i] = originalArray[i];
-        }
-        
-        // Add the new element to the end of the new array
-        newArray[newArray.length - 1] = newElement;
-        
-        // Print the new array using a for loop
-        System.out.println("New array with added element:");
-        for (int i = 0; i < newArray.length; i++) {
-            System.out.print(newArray[i] + " ");
-        }
-    }
-}*/
+
