d27defcba2ec37703e274172b134b6b5e3cf1782
Avantika Tayde <103492326+AvantikaTayde123@users.noreply.github.com>
2025-02-06 23:56:57 +0530

    Create TwoSum.java

---
+++ TwoSum.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+
+class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> numMap = new HashMap<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement)) {
+                return new int[]{numMap.get(complement), i}; // Return indices
+            }
+            numMap.put(nums[i], i); // Store the number and index
+        }
+        return new int[]{}; // Return empty array if no solution found
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        int[] result = twoSum(nums, target);
+        if (result.length > 0) {
+            System.out.println("Indices: [" + result[0] + ", " + result[1] + "]");
+        } else {
+            System.out.println("No solution found.");
+        }
+    }
+}
